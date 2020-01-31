9f7669435cdfbf481ec1ff5333ca26ed68f6a9f1
Horacio Herrera <hi@horacioh.com>
2020-01-31 02:16:07 +0100

    first commit. projecto ejemplo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Amplify, { API, graphqlOperation } from "aws-amplify";
+
+import awsconfig from "./aws-exports";
+import * as queries from "./graphql/queries";
+
+Amplify.configure(awsconfig);
 
 function App() {
+  const [todos, setTodos] = React.useState([]);
+
+  React.useEffect(() => {
+    async function init() {
+      const { data } = await API.graphql(graphqlOperation(queries.listTodos));
+      setTodos(data.listTodos.items);
+    }
+
+    init();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        {todos.map(todo => (
+          <li>
+            <input type="checkbox" value={todo.done} />
+            {`${todo.name} - ${todo.description}`}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
